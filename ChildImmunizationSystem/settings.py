4726fd3a118dba305b9c98e1117dcb7ca8da4ea0
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-11-20 20:34:14 +0530

    Add Login/Logout links in base.html

--- ChildImmunizationSystem/settings.py
+++ ChildImmunizationSystem/settings.py
@@ -119,6 +119,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'myowntestmail0@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_PASSWORD = 'cook!es@123'
+
+
+LOGIN_URL = '/account/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
