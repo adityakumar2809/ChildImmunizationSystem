bede3361a5afe3c487c066292f53e0cb2af6d8e1
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-11-20 19:43:38 +0530

    Add days_offset to vaccine model

--- data/models.py
+++ data/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=255)
+    days_offset = models.PositiveIntegerField()
 
     def __str__(self):
         return f'{self.name}'
