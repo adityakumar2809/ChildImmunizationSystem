2995e4886991e8da173df84c0b08b4cb1b83cc36
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-11-29 14:31:48 +0530

    Add notifications for Missed Immunization to Medical Agencies

--- ChildImmunizationSystem/views.py
+++ ChildImmunizationSystem/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 
 from beneficiary import models as ben_models
+from medical import models as med_models
 
 import datetime
 
@@ -16,4 +17,13 @@ def send_notifications(request):
     for notification in notifications:
         email_list = [notification.child_vaccine.child.parent.user.email, notification.child_vaccine.child.parent.medical_helper.user.email]
         send_mail('Immunization Date Coming Up', f'The Immunization via {notification.child_vaccine.vaccine.name} vaccine for {notification.child_vaccine.child.first_name} is scheduled on {notification.child_vaccine.scheduled_date}. Kindly ensure that the child is getting vaccinated. Thank you', 'myowntestmail0@gmail.com', email_list, fail_silently = True)
+
+    medical_agencies = med_models.MedicalAgency.objects.all()
+    for medical_agency in medical_agencies:
+        missed_vaccines = ben_models.ChildVaccine.objects.all().filter(child__parent__locality__medical_agency__pk__exact=medical_agency.pk ,scheduled_date__lt=datetime.date.today(), is_vaccinated__exact=False)
+        msg_str = 'The following immunizations were skipped in your area and are still not completed: \n'
+        for missed_vaccine in missed_vaccines:
+            msg_str = msg_str + f'{missed_vaccine.child.parent} -- {missed_vaccine.child} -- { missed_vaccine.vaccine }\n'
+            send_mail('Missed Immunization', msg_str, 'myowntestmail0@gmail.com', [medical_agency.user.email], fail_silently = True)
+           
     return redirect('home')
