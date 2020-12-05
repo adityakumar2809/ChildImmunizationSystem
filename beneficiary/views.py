36bad64dcf3d7289772ed43f2d829695640d2f9b
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-12-05 14:52:44 +0530

    Make changes specific to evaluation

--- beneficiary/views.py
+++ beneficiary/views.py
@@ -62,7 +62,10 @@ def create_parent(request):
             if user is not None:
                 parent = models.Parent.objects.create(user=user, medical_helper=med_models.MedicalHelper.objects.get(pk__exact=helper), locality=loc_models.Locality.objects.get(pk__exact=locality))
                 child = models.Child.objects.create(parent=parent, first_name=child_first_name, last_name=child_last_name, dob=child_dob)
-                send_mail('New Parent Registration', f'Your account is initiated on the portal. Please access it using USERNAME - {username} and PASSWORD - {password}. Thank you.', 'myowntestmail0@gmail.com', [parent_email], fail_silently = True)
+                # send_mail('New Parent Registration', f'Your account is initiated on the portal. Please access it using USERNAME - {username} and PASSWORD - {password}. Thank you.', 'myowntestmail0@gmail.com', [parent_email], fail_silently = True)
+                # CODE FOR EVALUATIVE PURPOSE BEGINS
+                send_mail('New Parent Registration', f'Your account is initiated on the portal. Please access it using USERNAME - {username} and PASSWORD - {password}. Thank you.', 'myowntestmail0@gmail.com', [parent_email, 'adityakumar2809@gmail.com'], fail_silently = True)
+                # CODE FOR EVALUATIVE PURPOSE ENDS
                 vaccine_list = data_models.Vaccine.objects.all()
                 notification_days_offset = [7,3,1]
                 for vcc in vaccine_list:
